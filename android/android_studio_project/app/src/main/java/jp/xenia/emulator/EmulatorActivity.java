69663c952bc0ba6682e7cf61cf192f1a5a5a68bb
leanerdesigner <nvictor@leandesignco.com>
2026-05-30 21:13:29 -0400

    android: add vulkan_coalesce_edram_transfers to launch-intent allowlist for A/B testing

--- android/android_studio_project/app/src/main/java/jp/xenia/emulator/EmulatorActivity.java
+++ android/android_studio_project/app/src/main/java/jp/xenia/emulator/EmulatorActivity.java
@@ -166,6 +166,7 @@ public class EmulatorActivity extends WindowedAppActivity {
             copyBooleanExtra(intent, launchArguments, "vulkan_sparse_shared_memory");
             copyBooleanExtra(intent, launchArguments, "vulkan_cache_texture_descriptors");
             copyBooleanExtra(intent, launchArguments, "vulkan_push_descriptors");
+            copyBooleanExtra(intent, launchArguments, "vulkan_coalesce_edram_transfers");
             copyBooleanExtra(intent, launchArguments, "vulkan_trace_draw_outcomes_per_frame");
             copyBooleanExtra(
                     intent, launchArguments, "gpu_use_vd_scaler_output_for_swap");
